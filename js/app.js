322b12a6be52acd92a6b11543afd2218fe5d6751
yurapa <yura.paray@gmail.com>
2017-10-19 18:30:52 +0300

    first version

---
+++ js/app.js
@@ -0,0 +1,15 @@
+var acc = document.getElementsByClassName("accordion");
+
+for (var i = 0; i < acc.length; i++) {
+    acc[i].onclick = function() {
+        for (var j = 0; j < acc.length; j++) {
+            if (acc[j] !== this) {
+                acc[j].classList.remove("active");
+                acc[j].nextElementSibling.classList.remove("show");
+            } else {
+                this.classList.toggle("active");
+                this.nextElementSibling.classList.toggle("show");
+            }
+        }
+    }
+}
