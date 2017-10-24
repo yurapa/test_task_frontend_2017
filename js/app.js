112aab03ebfe303f12bdd2b07739f010cf8d91a3
yurapa <yura.paray@gmail.com>
2017-10-25 00:41:59 +0300

    rewrite jquery to pure JS

--- js/app.js
+++ js/app.js
@@ -1,33 +1,77 @@
-var acc = $(".accordion");
+window.onload = function () {
 
-for (var i = 0; i < acc.length; i++) {
-    acc[i].onclick = function() {
-        for (var j = 0; j < acc.length; j++) {
-            if (acc[j] !== this) {
-                $( acc[j] ).removeClass("active");
-                $( acc[j] ).next().removeClass("show");
+    // ACCORDION
+    var accordion = document.querySelectorAll(".accordion__item-btn");
+
+    for (var i = 0; i < accordion.length; i++) {
+        accordion[i].onclick = function() {
+
+            var otherPanels = this.parentNode.parentNode.children;
+            for (var k = 0; k < otherPanels.length; k++) {
+                if (otherPanels[k] !== this.parentNode) {
+                    removeClass(otherPanels[k], 'active');
+                }
+            }
+
+            for (var j = 0; j < accordion.length; j++) {
+                if (accordion[j] == this) {
+                    if ( hasClass(accordion[j].parentNode, 'active') ) {
+                        removeClass(accordion[j].parentNode, 'active');
+                    } else {
+                        accordion[j].parentNode.className += ' active';
+                    }
+                }
+            }
+
+        }
+    }
+
+    // NAVIGATION
+    var navigation = document.querySelectorAll(".nav__dropdown-btn");
+
+    for (var i = 0; i < navigation.length; i++) {
+        navigation[i].onclick = function(event) {
+            event = event || window.event;
+            event.preventDefault ? event.preventDefault() : (event.returnValue = false);
+
+            var otherBtn = this.parentNode.parentNode.children;
+            for (var j = 0; j < otherBtn.length; j++) {
+                if (otherBtn[j] !== this.parentNode) {
+                    removeClass(otherBtn[j], 'active');
+                }
+            }
+
+            if ( hasClass(this.parentNode, 'active') ) {
+                removeClass(this.parentNode, 'active');
             } else {
-                $( this ).toggleClass( "active" );
-                $( this ).next().toggleClass( "show" );
+                this.parentNode.className += ' active';
+            }
+        }
+    }
+
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+    var target = event.target || event.srcElement; // IE9-11 fix
+    if ( !hasClass(target, 'nav__dropdown-btn') ) {
+        var dropdowns = document.getElementsByClassName('nav__dropdown');
+        for (var i = 0; i < dropdowns.length; i++) {
+            if ( hasClass( dropdowns[i].parentNode, 'active') ) {
+                removeClass( dropdowns[i].parentNode, 'active');
             }
         }
     }
 }
 
-$('ul.subMenu').hide();
+function hasClass(element, searchedClass) {
+    return (' ' + element.className + ' ').indexOf(' ' + searchedClass + ' ') > -1;
+}
 
-$('.navigation a.dropdown-btn').on('click', function (event) {
-    if ($(this).next().css('display') === 'none') {
-        $(this).next().slideDown('normal', function () {
-            // Animation complete.
-        });
-        $(this).addClass("active");
-    } else {
-        $(this).next().slideUp('fast', function () {
-            // Animation complete.
-        });
-        $(this).removeClass("active");
+function removeClass(el, classNameToRemove){
+    var elClass = ' ' + el.className + ' ';
+    while(elClass.indexOf(' ' + classNameToRemove + ' ') !== -1){
+         elClass = elClass.replace(' ' + classNameToRemove + ' ', '');
     }
-
-    event.preventDefault();
-});
+    el.className = elClass;
+}
